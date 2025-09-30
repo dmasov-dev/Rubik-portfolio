2bfe26e9373034e44af9e4aaa41c6d16553d7d22
dmasov-dev <dmasov@gmail.com>
2025-09-30 17:15:38 +0300

    Delete SERVER/Transfer directory

--- SERVER/Transfer/generate-hash.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const password = 'darinmasov9';
-const salt = bcrypt.genSaltSync(10);
-const hash = bcrypt.hashSync(password, salt);
-console.log('Hash for password "darinmasov9":', hash);
